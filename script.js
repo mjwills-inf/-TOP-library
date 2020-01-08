2c7b998fa1c26da844ed0fe6be56f9284138ba5d
mjwills-inf <mjwills@live.co.uk>
2020-01-08 13:26:11 +0000

    styling begun

--- script.js
+++ script.js
@@ -6,31 +6,35 @@ let myLibrary = [
 		title: 'The Hobbit',
 		author: 'J.R.R Tolkien',
 		pages: 310,
+		year: 1937,
 		read: true,
 	}, 
 	{
 		title: 'The Lord Of The Rings',
 		author: 'J.R.R Tolkien',
 		pages: 1137,
+		year: 1937,
 		read: true,
 	}, 
 	{
 		title: 'A Tale of Two Citites',
 		author: 'Charles Dickens',
 		pages: 341,
+		year: 1859,
 		read: false,
 	},
 ];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, year, read) {
 	this.title = title
 	this.author = author
 	this.pages = pages
+	this.year = year
 	this.read = read	
 }
 
-function addBookToLibrary(title, author, pages, read) {
-	let newBook = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages, year, read) {
+	let newBook = new Book(title, author, pages, year, read);
 	myLibrary.push(newBook)
 	render()
 }
@@ -38,12 +42,11 @@ function addBookToLibrary(title, author, pages, read) {
 
 render = () => {
 
-	let renderedCards = document.querySelectorAll('.book-card');
-	
+	// something to stop repeating entire array rendered again?
 	
 	for (i=0; i<myLibrary.length; i++) {
 
-
+	
 		
 		let newCard = document.createElement('div');
 		
@@ -51,6 +54,7 @@ render = () => {
 		let cardH3 = document.createElement('h3');
 		let cardH4 = document.createElement('h4');
 		let pages = document.createElement('p');
+		let year = document.createElement('p');
 		let readButton = document.createElement('button');
 
 		newCard.className = 'book-card';
@@ -59,30 +63,29 @@ render = () => {
 		cardH3.innerText = myLibrary[i].title;
 		cardH4.innerText = 	myLibrary[i].author;
 		pages.innerText = 'Pages: ' + myLibrary[i].pages;
+		year.innerText = 'Year: ' + myLibrary[i].year;
+		delButton.textContent = 'x';
 		
 		if (myLibrary[i].read) {
 			readButton.textContent = 'Read';
 			readButton.className = 'read';
 		} else {
-			readButton.textContent = 'Not Read';
-			readButton.className = 'not-read';
+			readButton.textContent = 'Unread';
+			readButton.className = 'unread';
 		}
 	
 		newCard.appendChild(delButton);
 		newCard.appendChild(cardH3);
 		newCard.appendChild(cardH4);
 		newCard.appendChild(pages);
+		newCard.appendChild(year)
 		newCard.appendChild(readButton);
 
-		cardContainer.appendChild(newCard);
+		cardContainer.prepend(newCard);
 
 	}
 
-	
 }
-
-
-
-addBookToLibrary('The Bible', 'Those guys et al', 1200, false )
+addBookToLibrary('The Bible', 'Those guys et al', 1200, 1611, false )
 
 
